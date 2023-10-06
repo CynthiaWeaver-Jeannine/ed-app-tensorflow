b40c785c34ac52fd3929cd25f4c8b989953fe728
cynthiajweaverdev <132491873+CynthiaWeaver-Jeannine@users.noreply.github.com>
2023-10-06 03:08:05 -0400

    add loadData function

--- scripts/load-data.ts
+++ scripts/load-data.ts
@@ -1,5 +1,23 @@
-function loadData(numUsers: number = 10) {
-    console.log(`executing load data.generating ${numUsers} users`)
+import { Client } from "pg";
+import { loadEnvConfig } from "@next/env";
+
+const projectDir = process.cwd();
+loadEnvConfig(projectDir);
+
+async function loadData(numUsers: number = 10) {
+    console.log(`executing load data. generating ${numUsers} users`)
+    const client = new Client({ 
+        user: process.env.POSTGRES_USER,
+        host: process.env.POSTGRES_HOST,
+        database: process.env.POSTGRES_NAME,
+        password: process.env.POSTGRES_PASSWORD,
+        port: parseInt(process.env.POSTGRES_PORT!),
+    });
+    await client.connect();
+
+    const res = client.query("select 1");
+    await client.end();
+
 }
 
-loadData(20);
+loadData();
